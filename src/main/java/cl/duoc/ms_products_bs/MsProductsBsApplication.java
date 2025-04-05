85086fde78f54279cda88105349826d87c25e9ad
juazocar <juazocar@profesor.duoc.cl>
2025-04-05 00:58:39 -0300

    mensaje commit

---
+++ src/main/java/cl/duoc/ms_products_bs/MsProductsBsApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_products_bs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProductsBsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProductsBsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_products_bs/MsProductsBsApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_products_bs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductsBsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
