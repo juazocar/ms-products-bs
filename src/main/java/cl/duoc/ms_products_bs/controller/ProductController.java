65ca6c53bd14f8a5d9f70df4bce5ca486ec4229a
juazocar <juazocar@profesor.duoc.cl>
2025-04-05 13:41:11 -0300

    mensaje

---
+++ src/main/java/cl/duoc/ms_products_bs/controller/ProductController.java
@@ -0,0 +1,26 @@
+package cl.duoc.ms_products_bs.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.duoc.ms_products_bs.model.dto.Product;
+import cl.duoc.ms_products_bs.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+    
+    
+}
---
+++ src/main/java/cl/duoc/ms_products_bs/service/ProductService.java
@@ -0,0 +1,24 @@
+package cl.duoc.ms_products_bs.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import cl.duoc.ms_products_bs.model.dto.Product;
+
+@Service
+public class ProductService {
+
+
+    public List<Product> getProducts() {
+        Product product1 = new Product(1, "producto 1", "descripcion 1", 10000);
+        Product product2 = new Product(2, "producto 2", "descripcion 2", 20000);
+        Product product3 = new Product(3, "producto 3", "descripcion 3", 30000);
+        List<Product> lista = new ArrayList<>();
+        lista.add(product1);
+        lista.add(product2);
+        lista.add(product3);
+        return lista;
+    }
+}
